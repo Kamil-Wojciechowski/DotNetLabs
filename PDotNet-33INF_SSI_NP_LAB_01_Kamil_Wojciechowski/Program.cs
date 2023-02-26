6374d825783ccd35d1d09bc6cb8217ae76890414
Kamil-Wojciechowski <wojciechowski.kamil1@outlook.com>
2023-02-26 08:12:47 +0100

    LAB-01

---
+++ PDotNet-33INF_SSI_NP_LAB_01_Kamil_Wojciechowski/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PDotNet_33INF_SSI_NP_LAB_01_Kamil_Wojciechowski
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
